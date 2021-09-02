bb7466fd77dc92758ba4d89cbf15e850beb106b7
akansha29 <akansha.pandey@brightlifecare.com>
2021-09-02 12:29:24 +0530

    changes

--- hc/settings.py
+++ hc/settings.py
@@ -156,12 +156,12 @@ USE_TZ = True
 TIME_ZONE = "UTC"
 LOCALE_PATHS = (os.path.join(BASE_DIR, "locale"),)
 
-SITE_ROOT = os.getenv("SITE_ROOT", "http://localhost:8000")
+SITE_ROOT = os.getenv("SITE_ROOT", "http://0.0.0.0:8000")
 SITE_NAME = os.getenv("SITE_NAME", "Mychecks")
 SITE_LOGO_URL = os.getenv("SITE_LOGO_URL")
 MASTER_BADGE_LABEL = os.getenv("MASTER_BADGE_LABEL", SITE_NAME)
 PING_ENDPOINT = os.getenv("PING_ENDPOINT", SITE_ROOT + "/ping/")
-PING_EMAIL_DOMAIN = os.getenv("PING_EMAIL_DOMAIN", "localhost")
+PING_EMAIL_DOMAIN = os.getenv("PING_EMAIL_DOMAIN", "0.0.0.0")
 PING_BODY_LIMIT = envint("PING_BODY_LIMIT", "10000")
 STATIC_URL = "/static/"
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
